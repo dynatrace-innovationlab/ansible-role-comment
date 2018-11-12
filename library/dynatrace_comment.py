1a356730993b3a722816b2f2a673af296918be8c
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2018-11-12 13:23:24 +0100

    indentation fixes

--- library/dynatrace_comment.py
+++ library/dynatrace_comment.py
@@ -59,7 +59,7 @@ EXAMPLES = '''
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import fetch_url
-#from ansible.module_utils.six.moves.urllib.parse import urlencode
+# from ansible.module_utils.six.moves.urllib.parse import urlencode
 import json
 
 # ===========================================
@@ -69,59 +69,59 @@ import json
 
 def main():
 
-  module = AnsibleModule(
-      argument_spec=dict(
-          tenant_url=dict(required=True),
-          api_token=dict(required=True),
-          problem_id=dict(required=True),
-          comment=dict(required=True),
-          user=dict(required=True),
-          context=dict(required=False)
-      ),
-      supports_check_mode=True
-  )
+    module = AnsibleModule(
+        argument_spec=dict(
+            tenant_url=dict(required=True),
+            api_token=dict(required=True),
+            problem_id=dict(required=True),
+            comment=dict(required=True),
+            user=dict(required=True),
+            context=dict(required=False)
+        ),
+        supports_check_mode=True
+    )
 
-  # build list of params
-  params = {}
+    # build list of params
+    params = {}
 
-  # set standard context
-  params["context"] = "Ansible"
+    # set standard context
+    params["context"] = "Ansible"
 
-  for item in ["comment", "user", "context"]:
-    if module.params[item]:
-      params[item] = module.params[item]
+    for item in ["comment", "user", "context"]:
+        if module.params[item]:
+            params[item] = module.params[item]
 
-  # If we're in check mode, just exit pretending like we succeeded
-  if module.check_mode:
-    module.exit_json(changed=True)
-
-  # Send the comment to Dynatrace and attach it to a problem
-  dt_url = module.params["tenant_url"] + "/api/v1/problem/details/" + module.params["problem_id"] + "/comments"
-  #data = urlencode(params)
-  headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Api-Token ' + module.params['api_token']
-  }
-
-  # FAIL FOR DEBUG PURPOSES - TO INSPECT PAYLOAD #####
-  #module.fail_json(msg=json.dumps(params))
-  #
-
-  ####
-  # SEND COMMENT EVENT TO DYNATRACE
-  ####
-  try:
-    response, info = fetch_url(module, dt_url, data=json.dumps(params), headers=headers)
-
-    if info['status'] in (200, 201):
+    # If we're in check mode, just exit pretending like we succeeded
+    if module.check_mode:
       module.exit_json(changed=True)
-    elif info['status'] == 401:
-      module.fail_json(msg="Token Authentification failed.")
-    else:
-      module.fail_json(msg="Unable to send comment to Dynatrace: %s" % info)
-  except Exception as e:
-    module.fail_json(msg="Failure: " + e.message)
+
+    # Send the comment to Dynatrace and attach it to a problem
+    dt_url = module.params["tenant_url"] + "/api/v1/problem/details/" + module.params["problem_id"] + "/comments"
+    #data = urlencode(params)
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Api-Token ' + module.params['api_token']
+    }
+
+    # FAIL FOR DEBUG PURPOSES - TO INSPECT PAYLOAD #####
+    #module.fail_json(msg=json.dumps(params))
+    #
+
+    ####
+    # SEND COMMENT EVENT TO DYNATRACE
+    ####
+    try:
+      response, info = fetch_url(module, dt_url, data=json.dumps(params), headers=headers)
+
+      if info['status'] in (200, 201):
+        module.exit_json(changed=True)
+      elif info['status'] == 401:
+        module.fail_json(msg="Token Authentification failed.")
+      else:
+        module.fail_json(msg="Unable to send comment to Dynatrace: %s" % info)
+    except Exception as e:
+      module.fail_json(msg="Failure: " + e.message)
 
 if __name__ == '__main__':
-    main()
 
+    main()
